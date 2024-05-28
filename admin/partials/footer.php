5070b9215d507d7630ac696e28894c1d6840014a
Belaloo <belal.alshmalya@gmail.com>
2024-05-28 18:32:52 +0300

    initiate the project and create database

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!--start footer section-->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+            2024 All right reserved, Developed by: <a href="#"> Belal Alshamlaia</a>
+        </p>
+    </div>
+</div>
+<!--end menu footer-->
+
+</body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/admin.css">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Food order</title>
+</head>
+<body>
+<!--start menu section-->
+<div class="menu text-center">
+    <div class="wrapper">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="manage_admin.php">Admins</a></li>
+            <li><a href="manage_category.php">Category</a></li>
+            <li><a href="manage_food.php">Food</a></li>
+            <li><a href="manage_orders.php">Orders</a></li>
+        </ul>
+    </div>
+</div>
+<!--end menu section-->
